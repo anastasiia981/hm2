3dbff69ef9d1e6a900065f439e01ccab2c314644
Anastasiia <anastasiia98@internet.ru>
2023-12-18 16:15:43 +0400

    Дз2

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы
+// в обоих регистрах. Сформируйте строку, в которой все
+// заглавные буквы заменены на строчные
+
+
+
+using System;
+
+class Program
+
+{
+    static void Main()
+    {
+
+        string str = "AbCdEfG";
+
+        string result = "";
+
+        foreach (char c in str)
+
+        {
+
+            if (char.IsUpper(c))
+            {
+
+                result += char.ToLower(c);
+
+            }
+            else
+            {
+        
+                result += c;
+
+            }
+
+        }
+
+        Console.WriteLine(result);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+        
